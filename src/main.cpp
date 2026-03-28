98d6422cfbead82e0d114812e1f0d844760822aa
Runar Grøneng <runargroneng@gmail.com>
2026-03-28 15:52:20 +0100

    Added libraries to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <ILI9488.h>
+#include <Adafruit_GFX.h>
 
 
 
@@ -6,14 +9,14 @@
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, OUTPUT);
+  pinMode(10, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(2, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
+digitalWrite(10, HIGH);  // change state of the LED by setting the pin to the HIGH voltage level
   delay(1000);                      // wait for a second
-digitalWrite(2, LOW);   // change state of the LED by setting the pin to the LOW voltage level
+digitalWrite(10, LOW);   // change state of the LED by setting the pin to the LOW voltage level
   delay(1000);   
 }
