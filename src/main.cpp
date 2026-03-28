40185a273289be631f55643d474e5426fe7c29d9
Adrian Solheim <adrsolheim@gmail.com>
2026-03-28 16:25:16 +0100

    Define pin variables for TFT display

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <Adafruit_GFX.h>
-#include <Adafruit_ST7789.h>
+#include <Adafruit_ST7735.h>
 
 
+#define TFT_CS         10
+#define TFT_RST        8
+#define TFT_DC         9
+#define TFT_MOSI       11
+#define TFT_MISO       13
+#define SerialDebugging true
+
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 // put function declarations here:
 
